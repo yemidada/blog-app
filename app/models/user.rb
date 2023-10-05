dc003483373135a9f67edb6b6c24f5e7b52283b1
yemidada <yemidada@gmail.com>
2023-10-05 12:04:34 +0100

    Update N+1 problem issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy, foreign_key: 'author_id'
   has_many :comments, dependent: :destroy, foreign_key: 'users_id'
   has_many :likes, dependent: :destroy, foreign_key: 'users_id'
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
+  
 
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
